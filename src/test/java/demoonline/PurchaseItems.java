02956d8c3821dd0bc7e3d43af7cc6f8ccce79ba0
Karan Chauhan <kchauhan@mycom-internal.com>
2020-03-03 00:45:14 +0530

    added steps of code

--- src/test/java/demoonline/PurchaseItems.java
+++ src/test/java/demoonline/PurchaseItems.java
@@ -2,7 +2,6 @@ package demoonline;
 
 import org.junit.runner.RunWith;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -16,61 +15,63 @@ public class PurchaseItems
    @Given("^User launches the application$")
    public void user_launches_the_application() throws Throwable
    {
-
+      /*Chrome driver launch code
+      and naviagtion to URl*/
    }
 
    @When("^User fills the web form and click on \"([^\"]*)\"$")
    public void user_fills_the_web_form_and_click_on_something(String strArg1) throws Throwable
    {
-
+    // Will use xpaths to locate input text field and use sendKeys to input data
    }
 
    @Then("^User deletes \"([^\"]*)\" product$")
    public void user_deletes_something_product(String strArg1) throws Throwable
    {
-      throw new PendingException();
+      //locate delete button and click
+      //driver.click()
    }
 
    @Then("^User assert the purchase Id and amount$")
    public void user_assert_the_purchase_id_and_amount() throws Throwable
    {
-      throw new PendingException();
+//use getText() to fetch data and assert with expected data using AssertThat
    }
 
    @And("^User clicks on \"([^\"]*)\" from the product categories list$")
    public void user_clicks_on_something_from_the_product_categories_list(String strArg1) throws Throwable
    {
-      throw new PendingException();
+     // locate element(by xpath,name,id etc) and click
    }
 
    @And("^User click \"([^\"]*)\"$")
    public void user_click_something(String strArg1) throws Throwable
    {
-      throw new PendingException();
+   // locate element(by xpath,name,id etc) and click
    }
 
    @And("^click on \"([^\"]*)\"$")
    public void click_on_something(String strArg1) throws Throwable
    {
-      throw new PendingException();
+   // locate element(by xpath,name,id etc) and click
    }
 
    @And("^User click on \"([^\"]*)\" button on the popup window$")
    public void user_click_on_something_button_on_the_popup_window(String strArg1) throws Throwable
    {
-      throw new PendingException();
+   // locate element(by xpath,name,id etc) and click
    }
 
    @And("^User click on the \"([^\"]*)\" button$")
    public void user_click_on_the_something_button(String strArg1) throws Throwable
    {
-      throw new PendingException();
+   // locate element(by xpath,name,id etc) and click
    }
 
    @And("^User clicks OK$")
    public void user_clicks_ok() throws Throwable
    {
-      throw new PendingException();
+   // locate element(by xpath,name,id etc) and click
    }
 
 }
